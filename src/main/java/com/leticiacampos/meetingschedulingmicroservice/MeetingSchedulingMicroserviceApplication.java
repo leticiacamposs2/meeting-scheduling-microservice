015ec32641fd9c20251711302722148e0eb7d67e
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2022-04-05 22:51:52 -0300

    initial structure with gradle

---
+++ src/main/java/com/leticiacampos/meetingschedulingmicroservice/MeetingSchedulingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.leticiacampos.meetingschedulingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingSchedulingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingSchedulingMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leticiacampos/meetingschedulingmicroservice/MeetingSchedulingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leticiacampos.meetingschedulingmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingSchedulingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
