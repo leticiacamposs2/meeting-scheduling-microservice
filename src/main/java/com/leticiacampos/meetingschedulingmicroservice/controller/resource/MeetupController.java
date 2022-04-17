93b582eb4b4ef22bdb39a55b216769fbf4145a7f
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2022-04-17 20:47:14 -0300

    create class meetup

---
+++ src/main/java/com/leticiacampos/meetingschedulingmicroservice/controller/resource/MeetupController.java
@@ -0,0 +1,67 @@
+package com.leticiacampos.meetingschedulingmicroservice.controller.resource;
+
+import com.leticiacampos.meetingschedulingmicroservice.controller.dto.MeetupDTO;
+import com.leticiacampos.meetingschedulingmicroservice.controller.dto.MeetupFilterDTO;
+import com.leticiacampos.meetingschedulingmicroservice.controller.dto.RegistrationDTO;
+import com.leticiacampos.meetingschedulingmicroservice.model.entity.Meetup;
+import com.leticiacampos.meetingschedulingmicroservice.model.entity.Registration;
+import com.leticiacampos.meetingschedulingmicroservice.service.MeetupService;
+import com.leticiacampos.meetingschedulingmicroservice.service.RegistrationService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/meetups")
+@RequiredArgsConstructor
+public class MeetupController {
+
+    private final MeetupService meetupService;
+    private final RegistrationService registrationService;
+    private final ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private Integer create(@RequestBody MeetupDTO meetupDTO) {
+
+        Registration registration = registrationService.getRegistrationByRegistrationAttribute(meetupDTO.getRegistrationAttribute())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
+
+        Meetup entity = Meetup.builder()
+                .registration(registration)
+                .event(meetupDTO.getEvent())
+                .meetupDate("17/04/2022")
+                .build();
+
+        entity = meetupService.save(entity);
+        return entity.getId();
+    }
+
+    @GetMapping
+    public Page<MeetupDTO> find(MeetupFilterDTO dto, Pageable pageRequest) {
+
+        Page<Meetup> result = meetupService.find(dto, pageRequest);
+        List<MeetupDTO> meetups = result
+                .getContent()
+                .stream()
+                .map(entity -> {
+                    Registration registration = entity.getRegistration();
+                    RegistrationDTO registrationDTO = modelMapper
+                            .map(registration, RegistrationDTO.class);
+
+                    MeetupDTO meetupDTO = modelMapper
+                            .map(entity, MeetupDTO.class);
+                    meetupDTO.setRegistration(registrationDTO);
+                    return meetupDTO;
+                }).collect(Collectors.toList());
+        return new PageImpl<MeetupDTO>(meetups, pageRequest, result.getTotalElements());
+    }
+}
---
+++ src/main/java/com/leticiacampos/meetingschedulingmicroservice/service/MeetupService.java
@@ -0,0 +1,22 @@
+package com.leticiacampos.meetingschedulingmicroservice.service;
+
+import com.leticiacampos.meetingschedulingmicroservice.controller.dto.MeetupFilterDTO;
+import com.leticiacampos.meetingschedulingmicroservice.model.entity.Meetup;
+import com.leticiacampos.meetingschedulingmicroservice.model.entity.Registration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface MeetupService {
+
+    Meetup save(Meetup meetup);
+
+    Optional<Meetup> getById(Integer id);
+
+    Meetup update(Meetup loan);
+
+    Page<Meetup> find(MeetupFilterDTO filterDTO, Pageable pageable);
+
+    Page<Meetup> getRegistrationsByMeetup(Registration registration, Pageable pageable);
+}
