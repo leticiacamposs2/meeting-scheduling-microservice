61c3d71c687addac2a16ccede2db17b6775a091a
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2022-04-17 22:55:09 -0300

    class registered meetup dto

---
+++ src/main/java/com/leticiacampos/meetingschedulingmicroservice/controller/dto/RegisteredMeetupDTO.java
@@ -0,0 +1,15 @@
+package com.leticiacampos.meetingschedulingmicroservice.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RegisteredMeetupDTO {
+
+    private Boolean registered;
+}
